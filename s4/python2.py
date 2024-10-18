6b4f2faa33f9a7686875c229356266b0c774106e
aappaapp <adenpun2000@gmail.com>
2024-10-18 20:48:01 +0800

    2 and 3

---
+++ s4/python2.py
@@ -0,0 +1,13 @@
+# 4E21 Aden
+# https://github.com/aappaapp/school/blob/main/s4/python2.py
+
+marks: int = int(input("請輸入你的考試分數："))
+
+if marks >= 90:
+  print("去日本旅行")
+elif marks >= 75:
+  print("去韓國旅行")
+elif marks >= 65:
+  print("去台灣省旅行")
+else:
+  print("上多幾堂英文堂")
